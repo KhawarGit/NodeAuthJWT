d50e8c0398c7575b6d11d9ad381a8829a48994ef
Khawar Khan <ma3533899@gmail.com>
2024-03-10 14:48:31 +0500

    refactor : refactored app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const cokkieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(cors({
 app.use(bodyParser.json())
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 module.exports = app;
