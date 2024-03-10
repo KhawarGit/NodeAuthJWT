380f74ae72bea4415cc703ca7a9558cd8d1f6cef
Khawar Khan <ma3533899@gmail.com>
2024-03-10 14:40:05 +0500

    Configured third party middlewares in app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const cokkieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
 const app = express();
+
+//Cogifuring
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(bodyParser.json())
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
 module.exports = app;
