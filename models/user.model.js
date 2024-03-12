8504c48fecc9d6e48342b506bce9d9d922f5c584
Khawar Khan <ma3533899@gmail.com>
2024-03-12 08:40:04 +0500

    feat: designed user Model

---
+++ models/user.model.js
@@ -0,0 +1,60 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        fullName: {
+            firstName: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            middleName:{
+                type: String,
+                trim: true,
+                default: null,
+            },
+            lastName: {
+                type: String,
+                required: true,
+                trim: true,
+            }
+        },
+        avatar: {
+            type: String, // cloudinary URL
+            required: true,
+        },
+        likedBooks: [
+            {
+                type: Schema.Types.ObjectId, // to reference an object
+                ref: "Book"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String,
+
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
