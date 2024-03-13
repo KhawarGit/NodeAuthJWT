a823d9e283d292eefb1879bb70df4d0cf6714422
Khawar Khan <ma3533899@gmail.com>
2024-03-14 01:44:59 +0500

    Added mongoose-aggregate-paginate-v2 to the project

--- models/book.model.js
+++ models/book.model.js
@@ -1,9 +1,12 @@
 import mongoose , { Schema, mongo } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const bookSchema = new Schema(
     {
 
     }
 );
+
+bookSchema.plugin(mongooseAggregatePaginate);
 
 export const Book = mongoose.model("Book", bookSchema)
