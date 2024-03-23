8fcde17e9a88baf07b2f3d398765fbf470b1f2f7
Khawar Khan <ma3533899@gmail.com>
2024-03-24 00:32:00 +0500

    Added fucntion skeleton to upload media on Cloud

--- services/cloudinary.service.js
+++ services/cloudinary.service.js
@@ -1,8 +1,18 @@
 import { v2 as cloudinary } from "cloudinary";
 const fs = require("fs");
          
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
-});
+cloudinary.config(
+  {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  }
+);
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+};
