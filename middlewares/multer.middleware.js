8c75382ffb18180458bcecaa6ce8994550ab6aef
Khawar Khan <ma3533899@gmail.com>
2024-03-31 19:51:45 +0500

    Added some comments

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -2,7 +2,11 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+      // file -> comes with request, can be handled by multer or express file handler.
       cb(null, "./public/temp")
+      // Here cb has two parameters 
+      // 1) Error Handler , which we currently provided as null.
+      // 2) the folder path to store that file.
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
