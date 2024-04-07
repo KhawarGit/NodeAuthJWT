8517431b248f8757bd03370ddcfbd89c9153a154
Khawar Khan <ma3533899@gmail.com>
2024-04-08 03:37:07 +0500

    Added comments

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "16kb"})) // for working with request body.
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser()) // for working with cookies.
 
 module.exports = app;
