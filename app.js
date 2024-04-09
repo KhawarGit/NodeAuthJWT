c3cdec2c003c45da3d59736daf4b61b16ed55887
Khawar Khan <ma3533899@gmail.com>
2024-04-09 19:37:30 +0500

    Added comments

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}));
+})); // configuring cors middleware
 app.use(express.json({limit: "16kb"})) // for working with request body.
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
