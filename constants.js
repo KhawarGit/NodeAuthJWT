d1c46231b2d097a96011c748b9c9da8d68236472
Khawar Khan <ma3533899@gmail.com>
2024-04-12 07:26:40 +0500

    Added comments

--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
+// Here we place all the constant variables which should be made public so they are not put in the .env file
 module.exports = {
     DB_NAME : "NodeAuth",
     PORT : process.env.PORT || 8000
